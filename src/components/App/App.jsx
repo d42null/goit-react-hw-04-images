a96e90d0c93990d5ba8e9eec0972d4c7345fa132
d42null <d2null@gmail.com>
2023-11-26 22:42:33 +0200

    fetchdata ImageGallery->App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,55 @@
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { Container } from './App.styled';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
-import { useState } from 'react';
+import { getImagesBySearchQuery } from 'services/pixabay-api';
+import { useState, useEffect } from 'react';
 export const App = () => {
   const [searchQ, setSearchQ] = useState('');
   const [page, setPage] = useState(1);
+  const [hits, setHits] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(0);
+
+  useEffect(() => {
+    if (searchQ === '') return;
+    fetchData();
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const data = await getImagesBySearchQuery(searchQ, page);
+        setHits(hits => [...hits, ...data.hits]);
+        if (page === 1) {
+          setTotalHits(data.totalHits);
+          setError(!data.totalHits ? { message: 'No results' } : null);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  }, [page, searchQ]);
   const onSearchSubmit = query => {
     setSearchQ(query);
     setPage(1);
+    setHits([]);
+    setTotalHits(0);
+    setError(null);
   };
   const onLoadMore = () => setPage(page => page + 1);
   return (
     <Container>
       <Searchbar onQuerySubmit={onSearchSubmit} />
-      {searchQ && (
+      {searchQ && hits.length > 0 && (
         <ImageGallery
-          searchQ={searchQ}
           page={page}
+          hits={hits}
+          loading={loading}
+          error={error}
+          totalHits={totalHits}
           onLoadMore={onLoadMore}
-        ></ImageGallery>
+        />
       )}
     </Container>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,71 +1,40 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect, useRef } from 'react';
 import { ErrorMsg, Gallery } from './ImageGallery.styled';
-import { getImagesBySearchQuery } from 'services/pixabay-api';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
 const PER_PAGE = 12;
-export const ImageGallery = ({ searchQ, page, onLoadMore }) => {
-  const [hits, setHits] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [totalHits, setTotalHits] = useState(0);
-  const didMount = useRef(false);
-
-  useEffect(() => {
-    if (!didMount.current) {
-      didMount.current = true;
-      return;
-    }
-    if (page === 1) {
-      setHits([]);
-      setTotalHits(0);
-      setError(null);
-    }
-    fetchData();
-    async function fetchData() {
-      setLoading(true);
-      try {
-        const data = await getImagesBySearchQuery(searchQ, page);
-        setHits(hits => [...hits, ...data.hits]);
-        if (page === 1) {
-          setTotalHits(data.totalHits);
-          setError(!data.totalHits ? { message: 'No results' } : null);
-        }
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  }, [page, searchQ]);
-
-  return (
-    <>
-      {hits.length > 0 && (
-        <Gallery>
-          {hits.map(({ id, webformatURL, largeImageURL, tags }) => (
-            <ImageGalleryItem
-              key={id}
-              webformatURL={webformatURL}
-              tags={tags}
-              largeImageURL={largeImageURL}
-            />
-          ))}
-        </Gallery>
-      )}
-      {error && <ErrorMsg>{error.message}</ErrorMsg>}
-      <Loader visible={loading} />
-      {totalHits > 0 && page * PER_PAGE < totalHits && !loading && (
-        <Button onLoadMore={onLoadMore}></Button>
-      )}
-    </>
-  );
-};
-
+export const ImageGallery = ({
+  page,
+  hits,
+  loading,
+  error,
+  totalHits,
+  onLoadMore,
+}) => (
+  <>
+    <Gallery>
+      {hits.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <ImageGalleryItem
+          key={id}
+          webformatURL={webformatURL}
+          tags={tags}
+          largeImageURL={largeImageURL}
+        />
+      ))}
+    </Gallery>
+    {error && <ErrorMsg>{error.message}</ErrorMsg>}
+    <Loader visible={loading} />
+    {totalHits > 0 && page * PER_PAGE < totalHits && !loading && (
+      <Button onLoadMore={onLoadMore}></Button>
+    )}
+  </>
+);
 ImageGallery.propTypes = {
-  searchQ: PropTypes.string.isRequired,
+  hits: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+  totalHits: PropTypes.number.isRequired,
   page: PropTypes.number.isRequired,
   onLoadMore: PropTypes.func.isRequired,
+  error: PropTypes.object,
 };
